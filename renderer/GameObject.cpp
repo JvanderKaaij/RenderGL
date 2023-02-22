51ee3ce78004b7dde31a656039afdd873dc91f17
JvanderKaaij <j.g.vanderkaaij@uva.nl>
2023-02-22 23:08:06 +0100

    WIP by adding ECS GameObject

---
+++ renderer/GameObject.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joey on 22/02/2023.
+//
+
+#include "GameObject.h"
---
+++ renderer/GameObject.h
@@ -0,0 +1,10 @@
+#ifndef RENDERGL_GAMEOBJECT_H
+#define RENDERGL_GAMEOBJECT_H
+
+
+class GameObject {
+
+};
+
+
+#endif //RENDERGL_GAMEOBJECT_H
