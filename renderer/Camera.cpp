452dc20a6173cbb88846eb0d13003013766c8d1b
Joey vd Kaaij <j.g.vanderkaaij@uva.nl>
2023-03-29 11:46:13 +0200

    Got Camera Movement Working With Quaternions.

--- renderer/Camera.cpp
+++ renderer/Camera.cpp
@@ -17,23 +17,16 @@ void Camera::Move(glm::vec3 dir) {
 }
 
 void Camera::Rotate(glm::vec3 dir) {
-    glm::mat4 pitchRotation = glm::rotate(glm::mat4(1.0f), dir.y, right);
-    glm::mat4 yawRotation = glm::rotate(glm::mat4(1.0f), dir.x, up);
-//
-//
-//    yawRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
-//    pitchRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
-//
-//    // Rotate quaternion around axis by 45 degrees
-//    yawRotation = RotateAroundAxis(up, glm::radians(dir.x * 100.0f), yawRotation);
-//    pitchRotation = RotateAroundAxis(right * yawRotation, glm::radians(dir.y * 100.0f), pitchRotation);
+    yawRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
+    pitchRotation = glm::quat(1.0f, 0.0f, 0.0f, 0.0f);
 
-//    std::cout << "(" << glm::eulerAngles(yawRotation).x << "," << glm::eulerAngles(yawRotation).y << "," << glm::eulerAngles(yawRotation).z << ")" << std::endl;
-//    std::cout << "(" << transform.rotation.x << "," << transform.rotation.y << "," << transform.rotation.z << ")" << std::endl;
+    // Rotate quaternion around axis by 45 degrees
+    yawRotation = RotateAroundAxis(up, glm::radians(dir.x * 100.0f), yawRotation);
+    pitchRotation = RotateAroundAxis(right * yawRotation, glm::radians(dir.y * 100.0f), pitchRotation);
 
-//    glm::quat rot = yawRotation * pitchRotation;
-//    transform.rotation += glm::eulerAngles(rot) / (float)(2.0f * M_PI);
-    viewMatrix = pitchRotation * yawRotation * viewMatrix;
+    glm::quat rot = yawRotation * pitchRotation;
+    transform.rotation += glm::eulerAngles(rot) / (float)(2.0f * M_PI);
+    viewMatrix = mat4_cast(rot) * viewMatrix; //Note that order of multiplication matters here!
 }
 
 glm::mat4 Camera::GetProjectionMatrix() {
